556d157a2cf2fa8391f8433d9ee1868b750c5464
ninesky00 <jiangzho@gmail.com>
2020-10-10 11:27:05 -0500

    add iteration3 get from category method and test

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -8,4 +8,8 @@ class Potluck
     def add_dish(new_dish)
         @dishes.push new_dish
     end
+
+    def get_all_from_category(category)
+        @dishes.select {|dish| dish.category == category}
+    end
 end
--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -7,7 +7,10 @@ class PotluckTest < Minitest::Test
         @potluck = Potluck.new("7-13-18")
         @couscous_salad = Dish.new("Couscous Salad", :appetizer)
         @cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
-     
+        @summer_pizza = Dish.new("Summer Pizza", :appetizer)
+        @roast_pork = Dish.new("Roast Pork", :entre)
+        @candy_salad = Dish.new("Candy Salad", :dessert)
+
     end
     
     def test_potluck_exist
@@ -23,13 +26,21 @@ class PotluckTest < Minitest::Test
         @potluck.add_dish(@couscous_salad)
         @potluck.add_dish(@cocktail_meatballs)
 
-        expected = []
-        expected << @couscous_salad
-        expected << @cocktail_meatballs
+        expected = [@couscous_salad, @cocktail_meatballs]
         assert_equal expected, @potluck.dishes
         assert_equal 2, @potluck.dishes.length
     end
 
-
-    
+    def test_show_dish_by_category_appetizer
+        @potluck.add_dish(@couscous_salad)
+        @potluck.add_dish(@summer_pizza)
+        @potluck.add_dish(@roast_pork)
+        @potluck.add_dish(@cocktail_meatballs)
+        @potluck.add_dish(@candy_salad)
+        
+        expected = [@couscous_salad, @summer_pizza] 
+        assert_equal expected, @potluck.get_all_from_category(:appetizer)
+        assert_equal @couscous_salad, @potluck.get_all_from_category(:appetizer).first
+        assert_equal "Couscous Salad", @potluck.get_all_from_category(:appetizer).first.name
+    end
 end
